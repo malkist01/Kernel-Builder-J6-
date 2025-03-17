5e746ddbc8c8f0bf76d5fbf4ee1ec2939d08bcec
malkist01 <164641045+malkist01@users.noreply.github.com>
2025-03-17 16:14:34 +0700

    Update caf.sh

--- scripts/caf.sh
+++ scripts/caf.sh
@@ -6,7 +6,7 @@ echo "Nuke previous toolchains"
 rm -rf toolchain out AnyKernel
 echo "cleaned up"
 echo "Cloning dependencies"
-git clone --depth=1 https://github.com/malkist01/linaro-arm-eabi-4.9.git -b master gcc-64
+git clone --depth=1 https://github.com/malkist01/malkist-gcc64.git -b master gcc-64
 echo "Done"
 if [ "$is_test" = true ]; then
      echo "Its alpha test build"
@@ -21,33 +21,11 @@ SHA=$(echo $DRONE_COMMIT_SHA | cut -c 1-8)
 IMAGE=$(pwd)/out/arch/arm/boot/Image.gz-dtb
 TANGGAL=$(date +'%H%M-%d%m%y')
 START=$(date +"%s")
-export CROSS_COMPILE="$(pwd)/gcc-64/bin/arm-eabi-"
+export CROSS_COMPILE="$(pwd)/gcc-64/bin/aarch64-linux-gnu-"
 export PATH="$(pwd)/gcc-64/bin:$PATH"
-export ARCH=arm
+export ARCH=arm64
 export KBUILD_BUILD_USER=malkist
 export KBUILD_BUILD_HOST=android
-# sticker plox
-function sticker() {
-    curl -s -X POST "https://api.telegram.org/bot$token/sendSticker" \
-        -d sticker="CAADBQADKwEAAkMQsyJtEJHSjxmH-wI" \
-        -d chat_id=$chat_id
-}
-# Send info plox channel
-function sendinfo() {
-    curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" \
-        -d chat_id="$chat_id" \
-        -d "disable_web_page_preview=true" \
-        -d "parse_mode=html" \
-        -d text="<b>ChipsKernel CAF EAS</b> CI Triggered%0ABuild started on <code>Drone CI/CD</code>%0AFor device <b>Samsung</b> (J6PRIMELTE)%0Abranch <code>$(git rev-parse --abbrev-ref HEAD)</code> (Android 10-11)%0AUnder commit <code>$(git log --pretty=format:'"%h : %s"' -1)</code>%0AUsing compiler: <code>$(${GCC}gcc --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')</code>%0AStarted on <code>$(date)</code>%0A<b>Build Status:</b> #Nightly"
-}
-# Send private info
-function sendpriv() {
-    curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" \
-        -d chat_id="$priv_id" \
-        -d "disable_web_page_preview=true" \
-        -d "parse_mode=html" \
-        -d text="ChipsKernel CI Started%0ADrone triggered by: <code>${DRONE_BUILD_EVENT}</code> event%0AJob name: Chips%0ACommit point: <a href='${DRONE_COMMIT_LINK}'>$(git log --pretty=format:'"%h : %s"' -1)</a>%0A<b>Pipeline jobs</b> <a href='https://cloud.drone.io/najahiiii/kernel_asus_sdm660/${DRONE_BUILD_NUMBER}'>here</a>"
-}
 # Push kernel to channel
 function push() {
     cd AnyKernel || exit 1
@@ -56,32 +34,18 @@ function push() {
         -F chat_id="$chat_id" \
         -F "disable_web_page_preview=true" \
         -F "parse_mode=html" \
-        -F caption="Build took $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s). | For <b>Samsung J6+</b> | <b>$(${GCC}gcc --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')</b>"
-}
-# Function upload logs to my own server paste
-function paste() {
-    cat build.log | curl -F 'chips=<-' https://chipslogs.herokuapp.com > link
-    HASIL="$(cat link)"
-}
-# Fin Error
-function finerr() {
-    curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" \
-        -d chat_id="$chat_id" \
-        -d "disable_web_page_preview=true" \
-        -d "parse_mode=markdown" \
-        -d text="Job Baking Chips throw an error(s)"
-    exit 1
+        -F caption="Build took $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s). | For <b>Samsung J6+</b>"
 }
 # Compile plox
 function compile() {
          CC=g++ \
-					  LD=ld \
-					  AR=ar \
-					  AS=as \
-					  NM=nm \
-					  OBJCOPY=objcopy \
-					  OBJDUMP=objdump \
-					  STRIP=strip \
+                                          LD=ld \
+                                          AR=ar \
+                                          AS=as \
+                                          NM=nm \
+                                          OBJCOPY=objcopy \
+                                          OBJDUMP=objdump \
+                                          STRIP=strip \
      make -C $(pwd) O=out teletubies_defconfig
      make -j8 -C $(pwd) O=out
 
@@ -96,7 +60,7 @@ function compile() {
 # Zipping
 zipping() {
     cd AnyKernel || exit 1
-    zip -r9 Teletubies-Arm"${CODENAME}"-"${DATE}".zip ./*
+    zip -r9 Teletubies-Eas"${CODENAME}"-"${DATE}".zip ./*
     cd ..
 }
 compile
