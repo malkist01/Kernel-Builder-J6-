90910942b481f297b11b55f262674bc563262183
malkist01 <164641045+malkist01@users.noreply.github.com>
2025-03-17 18:05:15 +0700

    Create arm.sh

---
+++ scripts/arm.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+rm -rf kernel
+git clone $REPO -b $BRANCH kernel 
+cd kernel
+echo "Nuke previous toolchains"
+rm -rf toolchain out AnyKernel
+echo "cleaned up"
+echo "Cloning dependencies"
+git clone --depth=1 https://github.com/malkist01/linaro-arm-eabi-4.9.git -b master gcc-64
+echo "Done"
+if [ "$is_test" = true ]; then
+     echo "Its alpha test build"
+     unset chat_id
+     unset token
+     export chat_id=${my_id}
+     export token=${nToken}
+else
+     echo "Its beta release build"
+fi
+SHA=$(echo $DRONE_COMMIT_SHA | cut -c 1-8)
+IMAGE=$(pwd)/out/arch/arm/boot/Image.gz-dtb
+TANGGAL=$(date +'%H%M-%d%m%y')
+START=$(date +"%s")
+export CROSS_COMPILE="$(pwd)/gcc-64/bin/arm-eabi-"
+export PATH="$(pwd)/gcc-64/bin:$PATH"
+export ARCH=arm
+export KBUILD_BUILD_USER=malkist
+export KBUILD_BUILD_HOST=android
+# Push kernel to channel
+function push() {
+    cd AnyKernel || exit 1
+    ZIP=$(echo *.zip)
+    curl -F document=@$ZIP "https://api.telegram.org/bot$token/sendDocument" \
+        -F chat_id="$chat_id" \
+        -F "disable_web_page_preview=true" \
+        -F "parse_mode=html" \
+        -F caption="Build took $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s). | For <b>Samsung J6+</b>"
+}
+# Compile plox
+function compile() {
+         CC=g++ \
+                                          LD=ld \
+                                          AR=ar \
+                                          AS=as \
+                                          NM=nm \
+                                          OBJCOPY=objcopy \
+                                          OBJDUMP=objdump \
+                                          STRIP=strip \
+     make -C $(pwd) O=out teletubies_defconfig
+     make -j8 -C $(pwd) O=out
+
+     if ! [ -a "$IMAGE" ]; then
+        finderr
+        exit 1
+    fi
+
+    git clone --depth=1 https://github.com/malkist01/anykernel3.git AnyKernel -b master
+    cp out/arch/arm/boot/Image.gz-dtb AnyKernel
+}
+# Zipping
+zipping() {
+    cd AnyKernel || exit 1
+    zip -r9 Teletubies-j6+"${CODENAME}"-arm"${DATE}".zip ./*
+    cd ..
+}
+compile
+zipping
+END=$(date +"%s")
+DIFF=$(($END - $START))
+push
