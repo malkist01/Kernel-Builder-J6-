1b4b7428e423d8f00bdc80d62e583a34f23953d7
malkist01 <164641045+malkist01@users.noreply.github.com>
2025-03-21 00:29:59 +0700

    Update arm64v2.sh

--- scripts/arm64v2.sh
+++ scripts/arm64v2.sh
@@ -40,7 +40,6 @@ function compile() {
     export PATH="$(pwd)/gcc-64/bin:$PATH"
     make -j$(nproc --all) O=out ARCH=arm64 j6primelte_defconfig
     make -j$(nproc --all) ARCH=arm64 O=out \
-                          CC=gcc \
                           CROSS_COMPILE=aarch64-linux-gnu- \
                           CROSS_COMPILE_ARM32=arm-linux-gnueabi-
 
