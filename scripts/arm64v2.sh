0554601f6e1abd9006e5d3d679e99dc5a832dcb7
malkist01 <164641045+malkist01@users.noreply.github.com>
2025-03-21 07:08:49 +0700

    Update arm64v2.sh

--- scripts/arm64v2.sh
+++ scripts/arm64v2.sh
@@ -6,7 +6,7 @@ echo "Nuke previous toolchains"
 rm -rf toolchain out AnyKernel
 echo "cleaned up"
 echo "Cloning dependencies"
-git clone --depth=1 https://github.com/malkist01/malkist-toolchain.git -b master gcc-64
+git clone --depth=1 https://github.com/malkist01/SM-aarch64-linux-android-4.9 -b master gcc-64
 echo "Done"
 if [ "$is_test" = true ]; then
      echo "Its alpha test build"
@@ -21,7 +21,7 @@ SHA=$(echo $DRONE_COMMIT_SHA | cut -c 1-8)
 IMAGE=$(pwd)/out/arch/arm64/boot/Image.gz-dtb
 TANGGAL=$(date +'%H%M-%d%m%y')
 START=$(date +"%s")
-export CROSS_COMPILE="$(pwd)/gcc-64/bin/aarch64-linux-gnu-"
+export CROSS_COMPILE="$(pwd)/gcc-64/bin/aarch64-linux-android-"
 export PATH="$(pwd)/gcc-64/bin:$PATH"
 export ARCH=arm64
 export KBUILD_BUILD_USER=malkist
