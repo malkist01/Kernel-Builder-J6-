395cfac7366964dfcaa68d13baa064b58b54851f
malkist01 <164641045+malkist01@users.noreply.github.com>
2025-03-21 07:14:28 +0700

    Update arm64v2.sh

--- scripts/arm64v2.sh
+++ scripts/arm64v2.sh
@@ -6,7 +6,7 @@ echo "Nuke previous toolchains"
 rm -rf toolchain out AnyKernel
 echo "cleaned up"
 echo "Cloning dependencies"
-git clone --depth=1 https://github.com/malkist01/SM-aarch64-linux-android-4.9 -b master gcc-64
+git clone --depth=1 https://github.com/malkist01/aarch64-linux-android-4.9-sm -b master gcc-64
 echo "Done"
 if [ "$is_test" = true ]; then
      echo "Its alpha test build"
